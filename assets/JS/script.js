ea49a6447ea40b8b3bb162d759dcb8c4bdd870ff
PykeSub <gastonalvarez0023@gmail.com>
2024-06-06 17:37:35 -0400

    Fin del Trabajo
    * Se termino de hacer casi todos los puntos, excepto el boton "Fuente" del punto 1 pero se logro realizar el boton contraste a la pagina.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -20,6 +20,8 @@ function contraste(){
     let eTextAreas = document.getElementsByClassName("textarea");
 
     if(ColorBody=="black"){
+        // Aqui basicamente es que si el body (cuerpo de la pagina) esta de color "Naranjo (Orange)", 
+        // los: H1, Inputs y el Text Area se vuelven de color "Negro".
         eBody.style.backgroundColor = "orange";
         for(let index = 0; index < eH1.length; index++) {
             const element = eH1[index];
@@ -36,6 +38,8 @@ function contraste(){
             element.style.bordercolor = "orange";
         }
     }
+    // Pero en este caso seria lo contrario es decir, que si el body esta de color "Negro (Black)",
+    // entonces los: H1, Inputs y Text Areas se volveran "Blancos (White)".
     else{
         eBody.style.backgroundColor = "black";
         for (let index = 0; index < eH1.length; index++) {
@@ -55,19 +59,24 @@ function contraste(){
     }
 }
 
-// Esta funcion lo que hace es validar si cumplen o no cada campo especifico.
+// La funcion de "Validar" cumple el rol de tomar el Id de cada uno de los Campos de las validaciones,
+// Para que pueda validar si esta cumplen o no.  
+// Si algun Campo del Formulario tiene algo escrito no te mostrara ninguna alerta.
+// Pero si no tiene nada en un campo especifico del Formulario, se pondria el borde de la casilla de color rojo.
+// Si no llegara a cumplir en algun campo especifico, entonces ademas de la alerta ya mencionada antes, 
+// tambien aparecera en el formulario de color "Rojo (Red)" el borde del cuadro como tambien el nombre de la seccion de dicho color.
 function validar(){
-    validarVacio("nombre");
-    validarVacio("precio");
-    validarLongitud("telefono");
-    validarVacio("direccion");
-    validarVacio("color");
-    validarVacio("mensaje");
+    validarNombre("nombre");
+    validarPrecio("precio");
+    validarLongitud("telefono", 9);
+    validarDireccion("direccion");
+    validarColor("color");
+    validarMensaje("mensaje");
+    validarFecha("fechaDeDisponibilidad");
+    validarCorreo("email");
 }
 
-// La funcion de Validar Vacio cumple el rol de si no tiene nada en un campo especifico se pondria de color rojo.
-// Pero si el campo tiene algo escrito en el, apareceria como de color verde para dar a entender que sitiene algo escrito. 
-function validarVacio(idCampo){
+function validarNombre(idCampo){
     let elemento3 = document.getElementById(idCampo);
     console.log(elemento3);
     let valor = elemento3.value;
@@ -77,7 +86,9 @@ function validarVacio(idCampo){
     console.log(valorSinEspacios.length);
     let eParrafoValor = document.getElementById("p"+ idCampo);
     if(valorSinEspacios==""){
+        document.getElementById("n").style.color = "red";
         console.log("Esta Vacio");
+        alert("El Campo 'Nombre' esta Vacio, Por favor verifique.");
         elemento3.style.borderColor = "red";
         eParrafoValor.style.display = "block";
     }
@@ -88,8 +99,146 @@ function validarVacio(idCampo){
     }
 }
 
-// La funcion de Validar la Longitud es lo mismo que la funcionde Validar Vacio, 
-//solo que con la diferencia en que esta funcion mide los caracteres ingresados en los campos si esta cumpliendo o no.
+function validarPrecio(idCampo){
+    let elemento3 = document.getElementById(idCampo);
+    console.log(elemento3);
+    let valor = elemento3.value;
+    console.log(valor);
+    console.log(valor.length);
+    let valorSinEspacios = valor.trim();
+    console.log(valorSinEspacios.length);
+    let eParrafoValor = document.getElementById("p"+ idCampo);
+    if(valorSinEspacios==""){
+        document.getElementById("p").style.color = "red";
+        console.log("Esta Vacio");
+        alert("El Campo 'Precio' esta Vacio, Por favor verifique.");
+        elemento3.style.borderColor = "red";
+        eParrafoValor.style.display = "block";
+    }
+    else{
+        console.log("Tiene Algo");
+        elemento3.style.borderColor = "green";
+        eParrafoValor.style.display = "none";
+    }
+}
+
+function validarDireccion(idCampo){
+    let elemento3 = document.getElementById(idCampo);
+    console.log(elemento3);
+    let valor = elemento3.value;
+    console.log(valor);
+    console.log(valor.length);
+    let valorSinEspacios = valor.trim();
+    console.log(valorSinEspacios.length);
+    let eParrafoValor = document.getElementById("p"+ idCampo);
+    if(valorSinEspacios==""){
+        document.getElementById("d").style.color = "red";
+        console.log("Esta Vacio");
+        alert("El Campo 'Direccion' esta Vacio, Por favor verifique.");
+        elemento3.style.borderColor = "red";
+        eParrafoValor.style.display = "block";
+    }
+    else{
+        console.log("Tiene Algo");
+        elemento3.style.borderColor = "green";
+        eParrafoValor.style.display = "none";
+    }
+}
+
+function validarColor(idCampo){
+    let elemento3 = document.getElementById(idCampo);
+    console.log(elemento3);
+    let valor = elemento3.value;
+    console.log(valor);
+    console.log(valor.length);
+    let valorSinEspacios = valor.trim();
+    console.log(valorSinEspacios.length);
+    let eParrafoValor = document.getElementById("p"+ idCampo);
+    if(valorSinEspacios==""){
+        document.getElementById("c").style.color = "red";
+        console.log("Esta Vacio");
+        alert("El Campo 'Color' esta Vacio, Por favor verifique.");
+        elemento3.style.borderColor = "red";
+        eParrafoValor.style.display = "block";
+    }
+    else{
+        console.log("Tiene Algo");
+        elemento3.style.borderColor = "green";
+        eParrafoValor.style.display = "none";
+    }
+}
+
+function validarMensaje(idCampo){
+    let elemento3 = document.getElementById(idCampo);
+    console.log(elemento3);
+    let valor = elemento3.value;
+    console.log(valor);
+    console.log(valor.length);
+    let valorSinEspacios = valor.trim();
+    console.log(valorSinEspacios.length);
+    let eParrafoValor = document.getElementById("p"+ idCampo);
+    if(valorSinEspacios==""){
+        document.getElementById("m").style.color = "red";
+        console.log("Esta Vacio");
+        alert("El Campo 'Mensaje' esta Vacio, Por favor verifique.");
+        elemento3.style.borderColor = "red";
+        eParrafoValor.style.display = "block";
+    }
+    else{
+        console.log("Tiene Algo");
+        elemento3.style.borderColor = "green";
+        eParrafoValor.style.display = "none";
+    }
+}
+
+function validarFecha(idCampo){
+    let elemento3 = document.getElementById(idCampo);
+    console.log(elemento3);
+    let valor = elemento3.value;
+    console.log(valor);
+    console.log(valor.length);
+    let valorSinEspacios = valor.trim();
+    console.log(valorSinEspacios.length);
+    let eParrafoValor = document.getElementById("p"+ idCampo);
+    if(valorSinEspacios==""){
+        document.getElementById("f").style.color = "red";
+        console.log("Esta Vacio");
+        alert("El Campo 'Fecha Disponibilidad' esta Vacio, Por favor verifique.");
+        elemento3.style.borderColor = "red";
+        eParrafoValor.style.display = "block";
+    }
+    else{
+        console.log("Tiene Algo");
+        elemento3.style.borderColor = "green";
+        eParrafoValor.style.display = "none";
+    }
+}
+
+function validarCorreo(idCampo){
+    let elemento3 = document.getElementById(idCampo);
+    console.log(elemento3);
+    let valor = elemento3.value;
+    console.log(valor);
+    console.log(valor.length);
+    let valorSinEspacios = valor.trim();
+    console.log(valorSinEspacios.length);
+    let eParrafoValor = document.getElementById("p"+ idCampo);
+    if(valorSinEspacios==""){
+        document.getElementById("e").style.color = "red";
+        console.log("Esta Vacio");
+        alert("El Campo 'Email' esta Vacio, Por favor verifique.");
+        elemento3.style.borderColor = "red";
+        eParrafoValor.style.display = "block";
+    }
+    else{
+        console.log("Tiene Algo");
+        elemento3.style.borderColor = "green";
+        eParrafoValor.style.display = "none";
+    }
+}
+
+// La funcion de Validar la Longitud es lo mismo que la funcionde Validar, 
+// Solo que con la diferencia en que esta funcion mide los caracteres ingresados en los campos si esta cumpliendo o no.
 function validarLongitud(idCampo, max){
     let elemento1 = document.getElementById(idCampo);
     console.log(elemento1);
@@ -106,12 +255,15 @@ function validarLongitud(idCampo, max){
     }
     else{
         console.log("No cumple");
+        alert("El campo Telefono tiene signos o mas de 9 numeros.");
         elemento1.style.borderColor = "red";
         eParrafoValor.style.display = "block";
+        document.getElementById("t").style.color = "red";
     }
 }
 
 // En esta Funcion cumple el rol de poder registrar lo que te pide cada campo en especifico que quieras poner para la pagina web y la base de datos.
+// Y al presionar el boton "Registrar", aparecera una alerta que dice que se habran registrado todos los cAmpos del Formulario.
 const registrar = ()=>{
     let eNombre = document.getElementById("nombre");
     let vNombre = eNombre.value;
@@ -125,6 +277,8 @@ const registrar = ()=>{
     let vColor = eColor.value;
     let eFechaDeDisponibilidad = document.getElementById("fechaDeDisponibilidad");
     let vFechaDeDisponibilidad = eFechaDeDisponibilidad.value;
+    let eEmail = document.getElementById("email");
+    let vEmail = eEmail.value;
     let eMensaje = document.getElementById("mensaje");
     let vMensaje = eMensaje.value;
     let objeto = {nombre : vNombre,
@@ -133,6 +287,7 @@ const registrar = ()=>{
                 direccion : vDireccion,
                 color : vColor,
                 fechaDeDisponibilidad : vFechaDeDisponibilidad,
+                email : vEmail,
                 mensaje : vMensaje
             }
     registrarDepartamento(objeto).then(()=>{
@@ -156,6 +311,7 @@ const obtenerDatos = ()=>{
             estructura += "<td>"+d.direccion+"</td>";
             estructura += "<td>"+d.color+"</td>";
             estructura += "<td>"+d.fechaDeDisponibilidad+"</td>";
+            estructura += "<td>"+d.email+"</td>";
             estructura += "<td>"+d.mensaje+"</td>";
             estructura += "<td><button id='UPD"+d.id+"'</button>Actualizar</td>";
             estructura += "<td><button id='DEL"+d.id+"'</button>Eliminar</td>";
@@ -172,9 +328,13 @@ const obtenerDatos = ()=>{
                 document.getElementById("UPDdireccion").value = d.direccion;
                 document.getElementById("UPDcolor").value = d.color;
                 document.getElementById("UPDfechaDeDisponibilidad").value = d.fechaDeDisponibilidad;
+                document.getElementById("UPDemail").value = d.email;
                 document.getElementById("UPDmensaje").value = d.mensaje;
                 document.getElementById("btnActualizar").value = d.id;
             })
+            // Aqui en el Boton "Eliminar" si lo presionas, aparecera una alerta donde te diga el "Nombre" y el "Precio" que quieres o no eliminar ese registro antes de que lo elimine al instante.
+            // Al presionar "Aceptar" en la alerta, le habras dado el visto bueno para que se elimine.
+            // Y aparecera otra alerta que diga que ya se habra eliminado. 
             let elemento2 = document.getElementById("DEL"+ d.id);
             elemento2.addEventListener("click", ()=>{
                 if(confirm("Se va a Eliminar a:\n"+ d.nombre + d.precio))(
@@ -189,7 +349,9 @@ const obtenerDatos = ()=>{
 }
 
 
-// En esta Funcion que se llama Actualizar le permite a uno el poder modificar un campo en especifico que se encuentra ya registrado en la tabla que uno quiera cambiar.
+// En esta Funcion que se llama "Actualizar" le permite a uno el poder modificar un campo en especifico que se encuentra ya registrado en la tabla que uno quiera cambiar.
+// Al "Actualizarlo" deberia de aparecer una ventana tipo alerta en la pagina que diga que si se actualizo.
+// Si NO aparece esa alerta en la pagina, no se actualizara la tabla y le aparecera el error en la consola.
 const actualizar = ()=>{
     let eNombre = document.getElementById("UPDnombre");
     let vNombre = eNombre.value;
@@ -203,6 +365,8 @@ const actualizar = ()=>{
     let vColor = eColor.value;
     let eFechaDeDisponibilidad = document.getElementById("UPDfechaDeDisponibilidad");
     let vFechaDeDisponibilidad = eFechaDeDisponibilidad.value;
+    let eEmail = document.getElementById("UPDemail");
+    let vEmail = eEmail.value;
     let eMensaje = document.getElementById("UPDmensaje");
     let vMensaje = eMensaje.value;
     let objeto = {nombre : vNombre,
@@ -211,6 +375,7 @@ const actualizar = ()=>{
                 direccion : vDireccion,
                 color : vColor,
                 fechaDeDisponibilidad : vFechaDeDisponibilidad,
+                email : vEmail,
                 mensaje : vMensaje
             }
     let id = document.getElementById("btnActualizar").value;
@@ -223,8 +388,9 @@ const actualizar = ()=>{
     });
 }
 
-// En esta Funcion cumple el rol de poder eliminar el registro que uno haya hecho. 
-// Al presionar el bototn te dira una alerta que diga si quieres o no elimiinar ese registro antes de que lo elimine al instante.
+// En esta Funcion cumple el rol de poder "eliminar" el registro que uno haya hecho del formulario a la tabla. 
+// aparecera una alerta que te dice que se habra eliminado de la tabla y en la tabla misma se actualizara al instante.
+// Pero si en la alerta presionas "Cancelar", entonces desaparecera esa alerta y no se eliminara de la tabla donde estan los registros.
 const eliminar = ()=>{
     let id = document.getElementById("btnEliminar").value;
     eliminarDepartamento(id).then(()=>{
@@ -236,7 +402,8 @@ const eliminar = ()=>{
     });
 }
 
-// Esta Funcion me permite que se vaya actualizando la pagina como tambien la tabla al instante, SIN necesidad de ir recargandola seguido.
+// Esta Funcion me permite que se vaya actualizando la pagina como tambien la tabla al instante, 
+// SIN necesidad de ir recargandola seguido.
 const limpiarTabla = (text)=>{
     let eNombre = document.getElementById("nombre");
     let ePrecio = document.getElementById("precio");
@@ -244,6 +411,7 @@ const limpiarTabla = (text)=>{
     let eDireccion = document.getElementById("direccion");
     let eColor = document.getElementById("color");
     let eFechaDeDisponibilidad = document.getElementById("fechaDeDisponibilidad");
+    let eEmail = document.getElementById("email");
     let eMensaje = document.getElementById("mensaje");
     eNombre.value = "";
     ePrecio.value = "";
@@ -251,5 +419,6 @@ const limpiarTabla = (text)=>{
     eDireccion.value = "";
     eColor.value = "";
     eFechaDeDisponibilidad.value = "";
+    eEmail.value = "";
     eMensaje.value = "";
 }
